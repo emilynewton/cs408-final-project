bed429704cd0f51fc6f38a403025fe697ae2b9a9
emilynewton <emilynewton@u.boisestate.edu>
2025-05-04 14:23:51 -0600

    adding tests

--- test/test.js
+++ test/test.js
@@ -1,4 +1,8 @@
 import {sayHello} from '../js/main.js';
+import {fetchLikeCount} from '../js/main.js'; 
+import {addTopic} from '../js/main.js'; 
+import {loadResponse} from '../js/main.js'; 
+import {deleteItem} from '../js/main.js'; 
 
 QUnit.module('hello', function() {
 
@@ -10,4 +14,50 @@ QUnit.module('hello', function() {
 
 });
 
+QUnit.module('fetch', function() {
+    
+    QUnit.test('making sure like count is properly fetched', function(assert) {
+        count = fetchLikeCount("fermat-likes", "https://ff0fnmu6n5.execute-api.us-east-2.amazonaws.com/items/fermat-likes"); 
+        assert.equal(count, "0"); 
+    });
 
+});
+
+QUnit.module('addTopic', function() {
+
+    QUnit.test('make sure the topic is added to the suggestions table', function(assert) {
+        
+    });
+
+});
+
+QUnit.module('loadResponse', function() {
+
+    QUnit.test('make sure that the responses are loaded to the site', function(assert) {
+
+    });
+
+});
+
+QUnit.module('deleteItem', function() {
+
+    QUnit.test('make sure that the delete item works', function(assert) {
+        assert.expect(2);
+        const done = assert.async();
+      
+        const originalXhr = window.XMLHttpRequest;
+        function MockXHR() {
+          this.open = function(method, url) {
+            assert.strictEqual(method, "DELETE");
+            assert.ok(url.includes("/suggestions/item-123"));
+          };
+          this.setRequestHeader = function() {};
+          this.send = function() { done(); };
+          this.onload = function() {};
+        }
+        window.XMLHttpRequest = MockXHR;
+        deleteItem("item-123");
+        window.XMLHttpRequest = originalXhr;        
+    });
+
+});
